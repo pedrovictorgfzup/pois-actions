3d6f4d79f3d801451ba9a03c0d3427f0068ec41e
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-05-15 16:39:57 -0300

    should break again

--- spec/models/poi_model_spec.rb
+++ spec/models/poi_model_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PointOfInterest do
-it "is valid with valid attributes" do 
-        poi = PointOfInterest.new
+it "is valid with valid attributes" do   poi = PointOfInterest.new
         poi.x = 2
         poi.y = 3
         poi.name = "Valid point of interest name"
