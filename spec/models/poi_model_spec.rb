40bfd5a4b6584b9887e8236b29a242ff62a68417
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-05-15 17:01:18 -0300

    should pass

--- spec/models/poi_model_spec.rb
+++ spec/models/poi_model_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PointOfInterest do
-it "is valid with valid attributes" do poi = PointOfInterest.new
+it "is valid with valid attributes" do 
+        poi = PointOfInterest.new
         poi.x = 2
         poi.y = 3
         poi.name = "Valid point of interest name"
@@ -9,14 +10,16 @@ it "is valid with valid attributes" do poi = PointOfInterest.new
         expect(poi).to be_valid
     end
 
-it "is invalid for negative coordinates" do poi = PointOfInterest.new
+it "is invalid for negative coordinates" do 
+        poi = PointOfInterest.new
         poi.name = "Valid point of interest name"
         poi.x = -2
         poi.y = -3
         expect(poi).to be_invalid
     end
 
-it "is invalid for names shorter than 3 characters" do poi = PointOfInterest.new
+it "is invalid for names shorter than 3 characters" do 
+        poi = PointOfInterest.new
         poi.name = "V"
         poi.x = 2
         poi.y = 3
