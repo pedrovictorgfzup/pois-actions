50713406e369fee3e6e1007452faa2e9a2deb6fd
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-05-15 16:02:00 -0300

    opa

--- test/ci_helper.rb
+++ test/ci_helper.rb
@@ -1,22 +1,13 @@
 require 'json'
-require 'open3'
 
 class Runner
 
     class << self
-        attr_reader :offenses
-
-        def initialize
-        @offenses = {}
-        end
-
         def execute
-            my_offense =  JSON.parse(`rubocop --format json`)
-            print my_offense["summary"], "\n"
-            master_offense = JSON.parse(master_offenses)
-            print master_offense["summary"], "\n"
+            print commit_offenses["summary"], "\n"
+            print previous_commit_offenses["summary"], "\n"
 
-            if my_offense["summary"]["offense_count"] > master_offense["summary"]["offense_count"]
+            if commit_offenses["summary"]["offense_count"] > previous_commit_offenses["summary"]["offense_count"]
                 print("DEU ERRADO")
                 exit 1
             else
@@ -24,35 +15,14 @@ class Runner
                 exit 0
             end
         end
-  
-        private
-  
-        attr_writer :offenses
-  
-        def files
-              @files ||= `git diff --name-only HEAD HEAD~1`.split("\n").select { |e| e =~ /.rb/ }
 
+        def commit_offenses
+            JSON.parse(`rubocop --format json`)
         end
   
-        def pr_offenses
-    #   pr_raw_data = `rubocop --auto-gen-config --exclude-limit 2000 --format j #{files.join(' ')}`
-    #   RubcopOutputParser.call(pr_raw_data)
-        end
-  
-        def master_offenses
-            # Open3.capture3('git checkout . && git checkout HEAD^')
+        def previous_commit_offenses
             `git checkout . && git checkout HEAD^`
-            print 'TENANDO PUXAR DO COMMIT ANTERIOR ', `git branch`, '\n'
-            `rubocop --format json`
-    #   RubcopOutputParser.call(master_raw_data)
-        end
-  
-        def new_offenses
-            offenses[:new_offenses] || {}
-        end
-  
-        def fixed_offenses
-            offenses[:fixed_offenses] || {}
+            JSON.parse(`rubocop --format json`)
         end
     end
 end
