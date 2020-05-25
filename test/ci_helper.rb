6c3fa454df12fd0c17718ee05b26366df347ae55
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-05-25 16:01:09 -0300

    parsing json

--- test/ci_helper.rb
+++ test/ci_helper.rb
@@ -26,14 +26,14 @@ class Runner
     end
 
     def pr_offenses
-      pr_raw_data = `rubocop --format json #{files.join(' ')}`
+      pr_raw_data = JSON.parse(`rubocop --format json #{files.join(' ')}`)
       new_offenses = pr_raw_data
     end
 
     def master_offenses
       Open3.capture3('git checkout . && git checkout HEAD^')
 
-      master_raw_data = `rubocop --format json #{files.join(' ')}`
+      master_raw_data = JSON.parse(`rubocop --format json #{files.join(' ')}`)
       old_offenses = master_raw_data
     end
 
