768113fc315935ae03d38d8b46c65c80ab5ec325
Pedro Victor Guerra de Figueiredo <pedrovictorgf@ZUP-2265.zup.corp>
2020-05-26 10:50:18 -0300

    look at this

--- test/ci_helper.rb
+++ test/ci_helper.rb
@@ -13,24 +13,39 @@ class Runner
 
     def execute
       pr_offenses = get_pr_offenses
-      print "PR OFFENSES: ", pr_offenses["summary"]["offense_count"], "\n"
-      print "PR: ", pr_offenses
       master_offenses = get_master_offenses
-      print "MASTER OFFENSES: ", master_offenses["summary"]["offense_count"], "\n"
-      print "MR: master_offenses"
 
-      report_hash = {}
+      pr_report_hash = {}
+      master_report_hash = {}
       
       if pr_offenses["summary"]["offense_count"] > master_offenses["summary"]["offense_count"]
         pr_offenses["files"].each do |file|
-          report_hash[file["path"]] = file["offenses"]
+          pr_report_hash[file["path"]] = {}
+
+          file["offenses"].each do |offense|
+            if pr_report_hash[file["path"]].has_key?(offense["cop_name"])
+              pr_report_hash[file["path"]][offense["cop_name"]] += 1
+            else
+              pr_report_hash[file["path"]][offense["cop_name"]] = 1
+            end
+          end
         end
 
         master_offenses["files"].each do |file|
-          report_hash[file["path"]] = report_hash[file["path"]] - file["offenses"]
+          master_report_hash[file["path"]] = {}
+
+          file["offenses"].each do |offense|
+            if master_report_hash[file["path"]].has_key?(offense["cop_name"])
+              master_report_hash[file["path"]][offense["cop_name"]] += 1
+            else
+              master_report_hash[file["path"]][offense["cop_name"]] = 1
+            end
+          end
         end
 
-        print "Olha só: ", report_hash, "\n\n\n"
+
+        print "Olha só pr: ", pr_report_hash, "\n\n\n"
+        print "Olha só master: ", master_report_hash, "\n\n\n"
         exit 1
       else
         print "Deu bom"
@@ -56,6 +71,14 @@ class Runner
     end
   end
 end
+
+
+
+
+
+
+
+
 
 
 
